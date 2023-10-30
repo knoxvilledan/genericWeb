0802e8f5dee298025965dadf2a57de4abbed1743
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-10-30 19:02:01 -0400

    fixed part 1

--- public/scripts.js
+++ public/scripts.js
@@ -1,21 +1,19 @@
+// Vars Dec
 let myName = "Daniel";
 let num1 = 10;
 let num2 = 20;
-
+// Boolean for later
 let isLearning = false
     console.log("keep going");
-
+// Computations
 let sum = num1 + num2;
-let difference = num2 - num1;
-    console.log(difference);
+let difference = num2 - num1;   
 let product = num1 * num2;
-    console.log(product);
-
-console.log(typeof myName);
-    console.log(typeof sum);
-
-let add = myName + sum;
-    console.log(add);
-// how could I have done this better?
-
-
+let nameAndSum = myName + ' ' + sum;
+// Logging
+console.log("Differnce:", difference);
+console.log("Product:", product);
+console.log("Type of myName:", typeof myName);
+console.log("Type of sum:", typeof sum);
+console.log("My myName and Sum:", nameAndSum);
+//this is what I found.
