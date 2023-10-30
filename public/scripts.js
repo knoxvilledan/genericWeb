3b3df50612538b669513f26d1d9a626e21d56ac1
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-10-30 18:48:54 -0400

    C T D2. It's going slow butttttt it's going!

---
+++ public/scripts.js
@@ -0,0 +1,21 @@
+let myName = "Daniel";
+let num1 = 10;
+let num2 = 20;
+
+let isLearning = false
+    console.log("keep going");
+
+let sum = num1 + num2;
+let difference = num2 - num1;
+    console.log(difference);
+let product = num1 * num2;
+    console.log(product);
+
+console.log(typeof myName);
+    console.log(typeof sum);
+
+let add = myName + sum;
+    console.log(add);
+// how could I have done this better?
+
+
