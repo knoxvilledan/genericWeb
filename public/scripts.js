e585aa5661010cb64bba822d4ef6774f0e37cbe5
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-10-30 22:41:40 -0400

    ct day2 upto this point

--- public/scripts.js
+++ public/scripts.js
@@ -17,3 +17,60 @@ console.log("Type of myName:", typeof myName);
 console.log("Type of sum:", typeof sum);
 console.log("My myName and Sum:", nameAndSum);
 //this is what I found.
+
+if (num1 > num2){
+    console.log("num1 is greater");
+}
+    else{
+        console.log("num2 is greater");
+    }
+
+let day = "Sunday"
+
+switch (day) {
+    case "Monday":
+        console.log("Start of the week!");
+        break;
+    case "Wednesday":
+        console.log("Hump day!");
+        break;
+    case "Sunday":
+        console.log("Rest day!");
+        break;
+    default:
+        console.log("Work Smarter");
+        break;
+}
+
+function greet(myName){
+    console.log(`Hello, ${myName}!`);
+}
+
+greet(`${myName}`);
+
+function multiply(num1, num2){
+   let answer = num1 * num2
+   console.log(answer);
+   
+}
+
+multiply(num1, num2);
+
+function factorial(n){
+    if (n === 0){
+        return 1;
+    } else {
+        return n * factorial(n -1);
+    }
+}
+console.log(factorial(6));
+// Totally got help from chatgpt on this factorial. 
+// Now I'm going to Drop a commit and then I'm going to see how I could have done this better.Section.
+
+
+
+
+
+
+
+
