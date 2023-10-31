48d4cbbccb308ef161fca34b12032ff0635bba7d
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-10-31 12:59:38 -0400

    paused to fix vs code

--- public/scripts.js
+++ public/scripts.js
@@ -1,2 +1,14 @@
-// The clock begins again. 
-// wait!! lol. that was a typo. 
+let myName = "Daniel";
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+let difference = num2 - num1;
+let product = num1 * num2;
+
+let isLearning = true;
+    console.log("Keep Growing and going");
+
+console.log(`Hello family this is the new and improve ${myName}`);
+console.log("sum is", sum);
+console.log("Difference is", difference);
+console.log("product is", )
