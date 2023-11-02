a0625c2c7cde5b4a6f4aa16499581d6d54e3ca88
knoxvilledan <138424723+knoxvilledan@users.noreply.github.com>
2023-11-01 21:43:17 -0400

    opt part 2 d3

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,7 @@
 //reset the plan. day 4. 400-
 //variables
 let myName = "Daniel";
+let age = 47;
 let num1 = 10;
 let num2 = 20;
 let isLearning = true;
@@ -8,7 +9,7 @@ let sum = num1 + num2;
 let difference = num2 - num1;
 let product = num1 * num2;
 
-console.log("Hello, Loved ones this is", myName);
+console.log("Hello, Loved ones this is", myName, "and I AM now", age, "years old.");
 console.log(num1);
 console.log(num2);
 console.log("Keep Going, Keep Moving Forward!");
@@ -74,6 +75,17 @@ console.log("Cherry is now in index:", fruits.indexOf("cherry"));
 for (let i =  0; i < fruits.length; i++){
     console.log(fruits[i]);
 }
-for (let i = 0; i < fruits.length; i--){
+
+for (let i = fruits.length -1; i >= 0; i--){
     console.log(fruits[i]);
 }
+// 900
+let num = (sum);
+while (num > 0){
+    console.log(num); num--;
+}
+
+let numBers = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+let totalSum = numBers.reduce((acc, curr)=> acc + curr, 0);
+console.log(totalSum)
+//0939 pause for optimization
